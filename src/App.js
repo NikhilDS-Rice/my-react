c414e985b30e2e1af42bf1003fba4d77aed778a2
NikhilDS-Rice <nd66@rice.edu>
2024-12-25 21:02:55 -0600

    Giftcard page added with first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Root from "./components/Theme"; //Light-Dark_Theme
+import Giftcard from "./Giftcard/Giftcard";
+
+
+function App() {
+  return (
+  <>
+    <Root /> 
+    <Giftcard />
+  </>
+      
+
+  );
+}
+
+export default App;
