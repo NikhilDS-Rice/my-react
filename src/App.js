a56f0fc23d63692ec6b4eaaccd5668fa5ffd870f
NikhilDS-Rice <nd66@rice.edu>
2024-12-26 04:42:04 -0600

    added mouse tracking

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import Root from "./components/Theme"; //Light-Dark_Theme
-import Giftcard from "./Giftcard/Giftcard";
-import Fetchdata from "./Fetchdata/Fetchdata";
-import Radiobuttons from "./Radiobuttons/Radiobuttons";
-
-function App() {
-  return (
-  <>
-    <Root /> 
-    <Giftcard />
-    <Fetchdata />
-    <Radiobuttons />
-  </>
-      
-
-  );
-}
-
-export default App;
-
-import Root from "./components/Theme"; //Light-Dark_Theme
 import Giftcard from "./Giftcard/Giftcard"; //Giftcard option
 import Fetchdata from "./Fetchdata/Fetchdata"; //Fetching data from Random.me API
 import Customhook from "./custom-hook/Custom-hook"; // Using custom-hooks
+import Radiobuttons from "./Radiobuttons/Radiobuttons"; //Using reactclone-components
+import Hoc from "./Hoc/Hoc"; //for mouse positions
 
 function App() {
   return (
@@ -30,6 +12,8 @@ function App() {
     <Giftcard />
     <Fetchdata />
     <Customhook />
+    <Radiobuttons />
+    <Hoc />
   </>
       
 
