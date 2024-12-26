696c24093bd764ff63d2f8a6eecc15bc52520715
NikhilDS-Rice <nd66@rice.edu>
2024-12-25 23:32:44 -0600

    fetching data updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Root from "./components/Theme"; //Light-Dark_Theme
 import Giftcard from "./Giftcard/Giftcard";
-
+import Fetchdata from "./Fetchdata/Fetchdata";
 
 function App() {
   return (
   <>
     <Root /> 
     <Giftcard />
+    <Fetchdata />
   </>
       
 
